e07583ee1f5ee8b8880d97270dc06aee557ae988
abu <abubakersuliman@outlook.com>
2023-05-06 22:30:32 +0400

    restaurant owner_name

--- restaurants_menus/serializers.py
+++ restaurants_menus/serializers.py
@@ -3,9 +3,11 @@ from .models import Restaurant, Menu
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
+    owner_name = serializers.CharField(source='owner.user.username', read_only=True)
+
     class Meta:
         model = Restaurant
-        fields = ['id', 'name', 'address']
+        fields = ['id', 'name', 'address', 'owner_name']
 
 
 class MenuSerializer(serializers.ModelSerializer):
