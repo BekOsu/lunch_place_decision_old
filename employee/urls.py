9f49161285936b5bb436cd0c58886b9d4feee6af
abu <abubakersuliman@outlook.com>
2023-05-08 12:13:09 +0400

    employee urls

--- employee/urls.py
+++ employee/urls.py
@@ -7,8 +7,8 @@ from django.urls import path
 
 urlpatterns = [
     # employees
-    path('list', EmployeeList.as_view(), name='employee_List'),
-    path('list/<int:pk>/', EmployeeDetail.as_view(), name='employee_detail'),
+    path('account', EmployeeList.as_view(), name='employee_List'),
+    path('account/<int:pk>/', EmployeeDetail.as_view(), name='employee_detail'),
     # voting
     path('votes/<str:version>/', VoteList.as_view(), name='vote_list'),
     path('votes/<str:version>/', VoteList.as_view(), name='vote_list'),
