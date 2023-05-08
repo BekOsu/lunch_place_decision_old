d67dafd63afc4b66332d45c283f0f6f7f520c4d6
abu <abubakersuliman@outlook.com>
2023-05-09 01:01:14 +0400

    refactor

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import RestaurantOwner, Restaurant, Menu
+from .models import RestaurantOwner, Restaurant
 
 admin.site.register(RestaurantOwner)
 admin.site.register(Restaurant)
-admin.site.register(Menu)
